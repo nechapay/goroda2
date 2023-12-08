8291341349e0816954a3f94f1cce5630cc40bd7e
nechapay <nechapay@list.ru>
2023-12-08 09:59:09 +0300

    first commit

---
+++ git.sh
@@ -0,0 +1,7 @@
+echo "# goroda2" >>README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/nechapay/goroda2.git
+git push -u origin main
